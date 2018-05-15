d5b9c71759c4fa485883e1a754b8335ec2515e37
ArSo77 <Bafik2017>
2018-05-15 23:24:47 +0200

    V0.3 - odczytano dane w tabelach

--- src/Ekran.vue
+++ src/Ekran.vue
@@ -1,80 +1,46 @@
 <template>
-
-
-
-    <v-app dark>
-
-        <header>
-<v-layout align-center column>
-    <img src="http://medicalonline.pl/files_med/606/581/t_39/20090216214059_Logo_as_1male_xl.jpg" height="112" width="316"/>
-<v-spacer></v-spacer>
-</v-layout>
-
-        </header>
-        <v-spacer></v-spacer>
-
     <v-layout column align-center>
-
-
-    <v-btn color="blue" @click="jso">JSON</v-btn>
+        <v-btn color="blue" @click="jso">JSON</v-btn>
         <v-btn color="blue" @click="war1=1">clear</v-btn>
 
 
 
-        <div>
-            <v-alert :value="true" type="success">
-                This is a success alert.
-            </v-alert>
 
-            <v-alert :value="true" type="info">
-                This is a info alert.
-            </v-alert>
 
-            <v-alert :value="true" type="warning">
-                This is a warning alert.
-            </v-alert>
 
-            <v-alert :value="true" type="error">
-                This is a error alert.
-            </v-alert>
-        </div>
+        <img src="http://medicalonline.pl/files_med/606/581/t_39/20090216214059_Logo_as_1male_xl.jpg" height="112" width="316"/>
 
 
 
 
-
-
-Alarmy:
+        Alarmy:
         <ul>
             <li v-for="alarm in alarmy">
                 {{alarm}}
             </li>
         </ul>
-Odczyty:
+        Odczyty:
         <ul>
             <li v-for="odczyt in odczyty">
                 {{odczyt}}
             </li>
         </ul>
-Rodzaje:
+        Rodzaje:
         <ul>
             <li v-for="rodzaj in rodzaje">
                 {{rodzaj}}
             </li>
         </ul>
 
-<p4>Specialny kod: {{kod}}</p4>
+        <p4>Specialny kod: {{kod}}</p4>
         <p4>Ilość gazów: {{iloscgazow}}</p4>
-
-
 
 
 
         <v-footer :fixed="fixed" app>
             <span>&copy;2018</span>
         </v-footer>
-        </v-layout>
-    </v-app>
+    </v-layout>
 </template>
 
 <script>
@@ -95,6 +61,7 @@ Rodzaje:
         },
         created: function(){
 
+
             this.kod=TekstJson["0x0"]["holding-registers"][13];
             this.iloscgazow=TekstJson["0x0"]["holding-registers"][14];
 
@@ -102,17 +69,28 @@ Rodzaje:
             for(i=0;i<6;i++){
                 this.alarmy[i]=TekstJson["0x0"]["holding-registers"][i+1];
             }
+
             for(i=0;i<6;i++){
                 this.odczyty[i]=TekstJson["0x0"]["holding-registers"][i+7];
             }
+
             for(i=0;i<6;i++){
                 this.rodzaje[i]=TekstJson["0x0"]["holding-registers"][i+15];
             }
+
+
+
+
         },
+
 
 
         methods: {
             jso() {
+
+
+
+
 
             }
         }
