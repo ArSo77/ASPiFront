5c993a93d0af3d8498d3f8b101cc8a3baa1d566e
ArSo77 <Bafik2017>
2018-05-15 22:52:09 +0200

    V0.3 - odczytano dane w tabelach

--- src/App.vue
+++ src/App.vue
@@ -6,37 +6,34 @@
 
 
 
-<ul>
-
-    <li>
-        Alarm1: {{alarm1}}
-    </li>
-
-    <li>
-        Alarm2: {{alarm2}}
-    </li>
-
-    <li>
-        Alarm3: {{alarm3}}
-    </li>
-
-    <li>
-        Alarm4: {{alarm3}}
-    </li>
-
-    <li>
-        Alarm5: {{alarm3}}
-    </li>
-
-    <li>
-        Alarm6: {{alarm3}}
-    </li>
-
-</ul>
-
-Specialny kod: {{kod}}
 
 
+    <img src="http://medicalonline.pl/files_med/606/581/t_39/20090216214059_Logo_as_1male_xl.jpg" height="112" width="316"/>
+
+
+
+
+Alarmy:
+        <ul>
+            <li v-for="alarm in alarmy">
+                {{alarm}}
+            </li>
+        </ul>
+Odczyty:
+        <ul>
+            <li v-for="odczyt in odczyty">
+                {{odczyt}}
+            </li>
+        </ul>
+Rodzaje:
+        <ul>
+            <li v-for="rodzaj in rodzaje">
+                {{rodzaj}}
+            </li>
+        </ul>
+
+<p4>Specialny kod: {{kod}}</p4>
+        <p4>Ilość gazów: {{iloscgazow}}</p4>
 
 
 
@@ -55,50 +52,38 @@ Specialny kod: {{kod}}
         data() {
             return {
                 war1:0,
-                alarm1:0,
-                alarm2:0,
-                alarm3:0,
-                alarm4:0,
-                alarm5:0,
-                alarm6:0,
-                odczyt1:0,
-                odczyt2:0,
-                odczyt3:0,
-                odczyt4:0,
-                odczyt5:0,
-                odczyt6:0,
+
                 kod:0,
                 iloscgazow:0,
-                rodgaz1:0,
-                rodgaz2:0,
-                rodgaz3:0,
-                rodgaz4:0,
-                rodgaz5:0,
-                rodgaz6:0,
+
+                alarmy:[],
+                odczyty:[],
+                rodzaje:[]
 
             }
         },
         created: function(){
-            this.alarm1 = TekstJson["0x0"]["holding-registers"][1];
-            this.alarm2 = TekstJson["0x0"]["holding-registers"][2];
-            this.alarm3 = TekstJson["0x0"]["holding-registers"][3];
-            this.alarm4 = TekstJson["0x0"]["holding-registers"][4];
-            this.alarm5 = TekstJson["0x0"]["holding-registers"][5];
-            this.alarm6 = TekstJson["0x0"]["holding-registers"][6];
-            this.odczyt1 = TekstJson["0x0"]["holding-registers"][7];
-            this.odczyt2 = TekstJson["0x0"]["holding-registers"][8];
-            this.odczyt3 = TekstJson["0x0"]["holding-registers"][9];
-            this.odczyt4 = TekstJson["0x0"]["holding-registers"][10];
-            this.odczyt5 = TekstJson["0x0"]["holding-registers"][11];
-            this.odczyt6 = TekstJson["0x0"]["holding-registers"][12];
-            this.kod = TekstJson["0x0"]["holding-registers"][13];
-            this.iloscgazow = TekstJson["0x0"]["holding-registers"][14];
-            this.rodgaz1 = TekstJson["0x0"]["holding-registers"][15];
-            this.rodgaz2 = TekstJson["0x0"]["holding-registers"][16];
-            this.rodgaz3 = TekstJson["0x0"]["holding-registers"][17];
-            this.rodgaz4 = TekstJson["0x0"]["holding-registers"][18];
-            this.rodgaz5 = TekstJson["0x0"]["holding-registers"][19];
-            this.rodgaz6 = TekstJson["0x0"]["holding-registers"][20];
+
+
+            this.kod=TekstJson["0x0"]["holding-registers"][13];
+            this.iloscgazow=TekstJson["0x0"]["holding-registers"][14];
+
+            var i=0;
+            for(i=0;i<6;i++){
+                this.alarmy[i]=TekstJson["0x0"]["holding-registers"][i+1];
+            }
+
+            for(i=0;i<6;i++){
+                this.odczyty[i]=TekstJson["0x0"]["holding-registers"][i+7];
+            }
+
+            for(i=0;i<6;i++){
+                this.rodzaje[i]=TekstJson["0x0"]["holding-registers"][i+15];
+            }
+
+
+
+
         },
 
 
