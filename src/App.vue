92c0f16e71df9364cfd91925f2e5377ab18c22e8
ArSo77 <arsobieraj@gmail.com>
2018-05-16 14:45:53 +0200

    V0.4

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,66 @@
 <template>
 
 
-
     <v-app dark>
-        <!--<img src="http://medicalonline.pl/files_med/606/581/t_39/20090216214059_Logo_as_1male_xl.jpg" height="112" width="316"/>-->
-
-MAIN APK
-        {{test}}
-        <v-btn @click="test+=1">Dodaj</v-btn>
-
-        <app-ekrany></app-ekrany>
-        <app-ekrany></app-ekrany>
+        <header></header>
 
 
-        <v-footer :fixed="fixed" app>
-            <img src="http://medicalonline.pl/files_med/606/581/t_39/20090216214059_Logo_as_1male_xl.jpg" height="30" width="90"/>
-            <v-spacer></v-spacer>
-            <span>&copy;2018</span>
 
-        </v-footer>
-        </v-layout>
+            <!--<img src="http://medicalonline.pl/files_med/606/581/t_39/20090216214059_Logo_as_1male_xl.jpg" height="112" width="316"/>-->
+
+
+
+
+        <v-btn @click="testf">{{test123}}</v-btn>
+
+
+
+
+        <v-container grid-list-md text-xs-center>
+            <v-layout row wrap>
+
+
+                <v-flex xs12>
+                    <v-card dark color="gray">
+                        <v-card-text class="px-0">
+                            <!--<img src="http://medicalonline.pl/files_med/606/581/t_39/20090216214059_Logo_as_1male_xl.jpg" height="112" width="316"/>-->
+                            <v-spacer></v-spacer>
+                            <h1>Monitor gazów medycznych</h1>
+                        </v-card-text>
+                    </v-card>
+                </v-flex>
+
+
+
+                <v-flex v-for="i in 3" :key="`4${i}`" xs6>
+                    <v-card dark color="primary">
+                        <v-card-text class="px-0">
+                            <app-ekrany :wartosciEnable="wartosciEkran" :alarmyEnable="alarmyEkran"></app-ekrany>
+                        </v-card-text>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+
+
+
+
+
+
+
+
+
+            <v-footer :fixed="fixed" app>
+
+                <img src="http://medicalonline.pl/files_med/606/581/t_39/20090216214059_Logo_as_1male_xl.jpg"
+                     height="30" width="90"/>
+                <v-spacer></v-spacer>
+                <v-btn @click="wartosciEkran=!wartosciEkran">Wartości:{{wartosciEkran}}</v-btn>
+                <v-btn @click="alarmyEkran=!alarmyEkran">Alarmy:{{alarmyEkran}}</v-btn>
+
+                <span>&copy;2018</span>
+                <!--<v-btn>Ustawienia</v-btn>-->
+            </v-footer>
     </v-app>
 </template>
 
@@ -31,24 +72,33 @@ MAIN APK
 
         data() {
             return {
-                test:1,
+                test123: 41,
+                wartosciEkran:true,
+                alarmyEkran:true
             }
         },
 
 
-        components:{
-          appEkrany: Ekran
+        components: {
+            appEkrany: Ekran
         },
 
-        created: function(){
+        created: function () {
         },
 
 
         methods: {
+            testf(){
+                this.test123=2;
+            }
         }
-
-
 
 
     }
 </script>
+
+<style scoped>
+    .redd {
+        background-color: red;
+    }
+</style>
--- src/Components/Ekran.vue
+++ src/Components/Ekran.vue
@@ -1,30 +1,51 @@
 <template>
     <v-layout column align-center>
-        <v-btn color="blue" @click="jso">JSON</v-btn>
-        <v-btn color="blue" @click="war1=1">clear</v-btn>
+<h3>
+Oddział: Pnumologia<v-spacer></v-spacer>
+        Pokój:421
+</h3>
+
+        <v-container grid-list-md text-xs-center>
+            <v-layout row wrap>
+
+                <v-flex v-for="i in 6" :key="`4${i}`" xs4>
+                    <v-card dark :color="kolorki[rodzaje[i-1]]">
+                        <v-card-text class="px-0">
+
+                           <h4> {{nazwaGazu[rodzaje[i-1]]}}</h4>
+
+                            <hr>
+                            <v-spacer></v-spacer>
+                            <div v-if="wartosciEnable">
+                            <h4>Wartość:</h4><h3>{{odczyty[i-1]}}</h3>
+                                <hr>
+                            </div>
+
+                            <div v-if="!alarmyEnable">
+                                <h4> {{nazwaAlarmu[alarmy[i-1]]}} </h4>
+                            </div>
+
+                        </v-card-text>
 
 
-        Alarmy:
-        <ul>
-            <li v-for="alarm in alarmy">
-                {{alarm}}
-            </li>
-        </ul>
-        Odczyty:
-        <ul>
-            <li v-for="odczyt in odczyty">
-                {{odczyt}}
-            </li>
-        </ul>
-        Rodzaje:
-        <ul>
-            <li v-for="rodzaj in rodzaje">
-                {{rodzaj}}
-            </li>
-        </ul>
+                        <div v-if="alarmyEnable">
+                        <v-alert :value="true" :type="nazwaAlertu[alarmy[i-1]]">
+                            <h4> {{nazwaAlarmu[alarmy[i-1]]}} </h4>
+                        </v-alert>
+                        </div>
 
-        <p4>Specialny kod: {{kod}}</p4>
-        <p4>Ilość gazów: {{iloscgazow}}</p4>
+
+
+
+
+                    </v-card>
+
+                </v-flex>
+
+            </v-layout>
+        </v-container>
+
+
 
 
 
@@ -47,8 +68,16 @@
                 iloscgazow:0,
                 alarmy:[],
                 odczyty:[],
-                rodzaje:[]
+                rodzaje:[],
+                kolorki: ['gray','red','gray','green','red','red','gray','red'],
+                nazwaGazu:['brak','Tlen','Podtlenek azotu','Próźnia','Dwutlenek węgla','Spręzone 5Bar','Spręzone 8Bar'],
+                nazwaAlarmu:['brak','Norma','Niskie','Wysokie','Zawór','AWARIA'],
+                nazwaAlertu:['success','success','warning','warning','warning',"error"]
             }
+        },
+        props:{
+          wartosciEnable: Boolean,
+          alarmyEnable: Boolean
         },
         created: function(){
 
@@ -76,3 +105,10 @@
         }
     }
 </script>
+
+<style scoped>
+
+    .redd{
+        background-color: red;
+    }
+</style>
